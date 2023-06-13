e6f7101be033a1ba19ab85e7b7ed89b0edebe718
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2023-06-13 17:02:54 +0200

    feat: update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,21 +7,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const simpleNFT = await hre.ethers.deployContract("simpleNFT");
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
+  await simpleNFT.waitForDeployment();
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `SimpleNFT deployed to ${simpleNFT.target}`
   );
 }
 
